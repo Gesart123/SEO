8ab17be5f28b97b5ba9a5aa1c8eb4453831dddab
Gesart123 <gesmll20@htl-shkoder.com>
2026-03-04 12:44:14 +0100

    add name und content by my project

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,9 +3,14 @@
     import background from '$lib/assets/background.jpg';
 </script>
 
+<svelte:head>
+    <title>Gesart's Webshop - Electronics for all</title>
+    <meta name="description" content="Welcome to Gesart's Webshop, your one-stop destination for all things electronic. Explore our wide range of gadgets, devices, and accessories designed to enhance your digital lifestyle. From cutting-edge smartphones to high-performance laptops, we have everything you need to stay connected and entertained. Shop with confidence and discover the latest in technology at Gesart's Webshop." />
+</svelte:head>
+
 <header class="h-[500px] bg-center bg-cover flex items-center justify-center"
     style="background-image: url({background})">
-    <h1 class="text-5xl">Electronic Devices</h1>
+    <h1 class="text-5xl font-bold text-white">Electronic Devices</h1>
 </header>
 
 <main>

3975e8f5e6636bf0182815509aa8ba26bad294af
Gesart123 <gesmll20@htl-shkoder.com>
2026-03-04 12:53:16 +0100

    do changes by footer and main

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -6,6 +6,8 @@
 <svelte:head>
     <title>Gesart's Webshop - Electronics for all</title>
     <meta name="description" content="Welcome to Gesart's Webshop, your one-stop destination for all things electronic. Explore our wide range of gadgets, devices, and accessories designed to enhance your digital lifestyle. From cutting-edge smartphones to high-performance laptops, we have everything you need to stay connected and entertained. Shop with confidence and discover the latest in technology at Gesart's Webshop." />
+    <meta property="og:title" content="Gesart's Webshop - Electronics for all" />
+    <meta property="og:image" content="{background}" />
 </svelte:head>
 
 <header class="h-[500px] bg-center bg-cover flex items-center justify-center"
@@ -13,10 +15,10 @@
     <h1 class="text-5xl font-bold text-white">Electronic Devices</h1>
 </header>
 
-<main>
+<main class="min-h-[300px]">
 
 </main>
 
-<footer>
-
+<footer class="h-[300px] bg-blue-950 text-white flex items-center justify-center">
+    <p>Test-Page for SEO</p>
 </footer>
